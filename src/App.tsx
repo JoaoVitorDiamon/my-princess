d033c6ccac227a9fbc8527f8b777974b08a5cadb
Joao Vitor <joaodiamon19@icloud.com>
2026-07-03 22:15:48 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { FinalChapterPage } from './pages/FinalChapterPage'
 import { ChapterFourPage } from './pages/ChapterFourPage'
 import { ChapterFivePage } from './pages/ChapterFivePage'
@@ -6,16 +7,29 @@ import { ChapterThreePage } from './pages/ChapterThreePage'
 import { ChapterTwoPage } from './pages/ChapterTwoPage'
 import { HomePage } from './pages/HomePage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/capitulo-2" element={<ChapterTwoPage />} />
-      <Route path="/capitulo-3" element={<ChapterThreePage />} />
-      <Route path="/capitulo-4" element={<ChapterFourPage />} />
-      <Route path="/capitulo-5" element={<ChapterFivePage />} />
-      <Route path="/capitulo-final" element={<FinalChapterPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/capitulo-2" element={<ChapterTwoPage />} />
+        <Route path="/capitulo-3" element={<ChapterThreePage />} />
+        <Route path="/capitulo-4" element={<ChapterFourPage />} />
+        <Route path="/capitulo-5" element={<ChapterFivePage />} />
+        <Route path="/capitulo-final" element={<FinalChapterPage />} />
+      </Routes>
+    </>
   )
 }
 
